e8b53e3cd5ca935c1082778e88ee5186a231b66b
Mario Di Vece <mario@unosquare.com>
2016-11-07 07:44:50 -0600

    initial refactoring

---
+++ Unosquare.FFplayDotNet/FFmpeg.Autogen.Additions.cs
@@ -0,0 +1,25 @@
+﻿namespace FFmpeg.AutoGen
+{
+    using System.Runtime.InteropServices;
+
+    public unsafe static partial class ffmpeg
+    {
+        /// <summary>
+        /// Gets the current time in microseconds since Jan 1, 1970
+        /// </summary>
+        /// <returns></returns>
+        [DllImport(libavutil, EntryPoint = "av_gettime", CallingConvention = CallingConvention.Cdecl)]
+        public static extern long av_gettime();
+
+        /// <summary>
+        /// Get the current time in microseconds since some unspecified starting point.
+        /// On platforms that support it, the time comes from a monotonic clock
+        /// This property makes this time source ideal for measuring relative time.
+        /// The returned values may not be monotonic on platforms where a monotonic
+        /// clock is not available.
+        /// </summary>
+        /// <returns></returns>
+        [DllImport(libavutil, EntryPoint = "av_gettime_relative", CallingConvention = CallingConvention.Cdecl)]
+        public static extern long av_gettime_relative();
+    }
+}

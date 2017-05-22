ed5ab76b165ad3dd3f2e07536e4e14b92568bb9e
Mario Di Vece <mario@unosquare.com>
2017-05-22 13:03:50 -0500

    better playback controller organization

--- Unosquare.FFplayDotNet/PlaybackController.cs
+++ Unosquare.FFplayDotNet/PlaybackController.cs
@@ -10,32 +10,47 @@
 
     public class PlaybackController
     {
+        #region Constants
+
+        private const int MaxPacketBufferLength = 1024 * 1024 * 4;
+        private const int PacketReadBatchCount = 10;
+        private const int MaxFrameQueueCount = 24;
+
+        #endregion
+
+        #region Private Members
+
         private MediaContainer Container;
         private Clock Clock = new Clock();
 
         private MediaFrameQueue Frames = new MediaFrameQueue();
-        private const int MaxPacketBufferLength = 1024 * 1024 * 4;
-        private const int MaxFrameQueueCount = 24;
-        private const int PacketBatchCount = 10;
-
         private readonly Dictionary<MediaType, MediaBlockBuffer> BlockBuffers = new Dictionary<MediaType, MediaBlockBuffer>();
         private readonly Dictionary<MediaType, int> BlockBufferCounts = new Dictionary<MediaType, int>()
         {
-            { MediaType.Video, 12 },
-            { MediaType.Audio, 24 },
-            { MediaType.Subtitle, 24 }
+            { MediaType.Video, MaxFrameQueueCount / 2 },
+            { MediaType.Audio, MaxFrameQueueCount },
+            { MediaType.Subtitle, MaxFrameQueueCount }
         };
 
-        private readonly MediaBlockBuffer BlockBuffer;
+        private readonly MediaBlockBuffer MainBlockBuffer;
 
-        private ConfiguredTaskAwaitable PacketReadingTask;
+        private Task PacketReadingTask;
         private readonly CancellationTokenSource PacketReadingCancel = new CancellationTokenSource();
         private readonly ManualResetEventSlim PacketReadingCycle = new ManualResetEventSlim(false);
         private readonly ManualResetEventSlim PacketReadingExit = new ManualResetEventSlim(false);
 
-        private ConfiguredTaskAwaitable FrameDecodingTask;
+        private Task FrameDecodingTask;
 
+        private Task BlockRenderingTask;
 
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PlaybackController"/> class.
+        /// </summary>
+        /// <param name="container">The container.</param>
         public PlaybackController(MediaContainer container)
         {
             Container = container;
@@ -43,8 +58,25 @@
                 BlockBuffers[component.MediaType] =
                     new MediaBlockBuffer(BlockBufferCounts[component.MediaType], component.MediaType);
 
-            BlockBuffer = BlockBuffers[Container.Components.Main.MediaType];
+            MainBlockBuffer = BlockBuffers[Container.Components.Main.MediaType];
         }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets a value indicating whether this instance can read more packets.
+        /// </summary>
+        private bool CanReadMorePackets { get { return Container.IsAtEndOfStream == false; } }
+
+        private bool CanReadMoreFrames { get { return Container.Components.PacketBufferLength > 0 || CanReadMorePackets; } }
+
+        private bool CanReadMoreBlocks { get { return Frames.Count() > 0 || CanReadMoreFrames || CanReadMorePackets; } }
+
+        #endregion
+
+        #region Methods
 
         private void AddNextBlock()
         {
@@ -60,75 +92,21 @@
             frame.Dispose();
         }
 
-        public void Test()
+        private void BufferBlocks(int packetBufferLength, bool setClock)
         {
-            PacketReadingTask = RunPacketReadingTask();
-            FrameDecodingTask = RunFrameDecodingTask();
+            // Buffer some packets
+            while (CanReadMorePackets && Container.Components.PacketBufferLength < packetBufferLength)
+                PacketReadingCycle.Wait(5);
 
-            while (BlockBuffer.Count == 0 ||
-                (BlockBuffer.CapacityPercent < 0.5d && Container.IsAtEndOfStream == false))
+            // Buffer some blocks
+            while (CanReadMoreBlocks && MainBlockBuffer.CapacityPercent < 0.5d)
             {
                 PacketReadingCycle.Wait(5);
                 AddNextBlock();
             }
 
-            var lastRenderTime = TimeSpan.MinValue;
-            var clockPosition = Clock.Position;
-            var hasRendered = false;
-            var renderIndex = -1;
-            MediaBlock renderBlock = null;
-
-            Clock.Position = BlockBuffer.RangeStartTime;
-            Clock.Play();
-
-            while (true)
-            {
-
-                clockPosition = Clock.Position;
-                hasRendered = false;
-                renderIndex = BlockBuffer.IndexOf(clockPosition);
-                renderBlock = BlockBuffer[renderIndex];
-
-                if (BlockBuffer.IsInRange(clockPosition) == false)
-                {
-                    $"SYNC ERROR - Setting CLK from {Clock.Position.Debug()} to {BlockBuffer.RangeStartTime.Debug()}".Warn(typeof(MediaContainer));
-                    Clock.Position = BlockBuffer.RangeStartTime;
-                }
-
-                // render the frame if we have not rendered
-                if (renderBlock.StartTime != lastRenderTime)
-                {
-                    lastRenderTime = renderBlock.StartTime;
-                    hasRendered = true;
-                    RenderBlock(renderBlock, clockPosition, renderIndex);
-                }
-
-                while (hasRendered || renderIndex + 1 > BlockBuffer.Capacity / 2)
-                {
-                    hasRendered = false;
-                    renderIndex = BlockBuffer.IndexOf(clockPosition);
-                    AddNextBlock();
-
-                    if (Frames.Count() == 0)
-                        break;
-                }
-
-
-                if (Container.IsAtEndOfStream && Container.Components.PacketBufferCount == 0
-                    && Frames.Count() == 0 && renderIndex == BlockBuffer.Count - 1)
-                {
-                    // Rendered all and nothing else to read
-                    Clock.Pause();
-                    break;
-                }
-
-                Thread.Sleep(2);
-            }
-
-            $"Finished rendering everything!".Warn(typeof(MediaContainer));
-
-            PacketReadingExit.Wait();
-
+            if (setClock)
+                Clock.Position = MainBlockBuffer.RangeStartTime;
         }
 
         private void RenderBlock(MediaBlock block, TimeSpan clockPosition, int renderIndex)
@@ -137,14 +115,30 @@
             $"BLK: {block.StartTime.Debug()} | CLK: {clockPosition.Debug()} | DFT: {drift.Debug()} | RIX: {renderIndex,6} | FQ: {Frames.Count(),6} | PQ: {Container.Components.PacketBufferLength / 1024d,6:0.00} KB".Info(typeof(MediaContainer));
         }
 
+        #endregion
+
+        public void Test()
+        {
+            PacketReadingTask = RunPacketReadingTask();
+            FrameDecodingTask = RunFrameDecodingTask();
+            BlockRenderingTask = RunBlockRenderingTask();
+
+            BlockRenderingTask.Wait();
+            $"Finished rendering everything!".Warn(typeof(MediaContainer));
+
+        }
+
+        #region Task Runners
+
         /// <summary>
         /// Runs the read task which keeps a packet buffer healthy.
         /// </summary>
-        private ConfiguredTaskAwaitable RunPacketReadingTask()
+        private Task RunPacketReadingTask()
         {
             return Task.Run(() =>
             {
                 PacketReadingExit.Reset();
+                var packetsRead = 0;
 
                 while (!PacketReadingCancel.IsCancellationRequested)
                 {
@@ -152,9 +146,9 @@
                     PacketReadingCycle.Reset();
 
                     // Read a bunch of packets at a time
-                    var packetsRead = 0;
-                    while (!Container.IsAtEndOfStream
-                        && packetsRead < PacketBatchCount
+                    packetsRead = 0;
+                    while (CanReadMorePackets
+                        && packetsRead < PacketReadBatchCount
                         && Container.Components.PacketBufferLength < MaxPacketBufferLength)
                     {
                         Container.Read();
@@ -163,7 +157,7 @@
 
                     PacketReadingCycle.Set();
 
-                    if (Container.Components.PacketBufferLength > MaxPacketBufferLength || Container.IsAtEndOfStream)
+                    if (!CanReadMorePackets || Container.Components.PacketBufferLength > MaxPacketBufferLength)
                         Thread.Sleep(1);
 
                 }
@@ -171,10 +165,10 @@
                 PacketReadingCycle.Set();
                 PacketReadingExit.Set();
 
-            }, PacketReadingCancel.Token).ConfigureAwait(false);
+            }, PacketReadingCancel.Token);
         }
 
-        private ConfiguredTaskAwaitable RunFrameDecodingTask()
+        private Task RunFrameDecodingTask()
         {
             return Task.Run(() =>
             {
@@ -194,7 +188,70 @@
                     }
                 }
 
-            }).ConfigureAwait(false);
+            });
         }
+
+        private Task RunBlockRenderingTask()
+        {
+            return Task.Run(() =>
+            {
+                var lastRenderTime = TimeSpan.MinValue;
+                var clockPosition = Clock.Position;
+                var hasRendered = false;
+                var renderIndex = -1;
+                MediaBlock renderBlock = null;
+
+                BufferBlocks(MaxPacketBufferLength / 8, true);
+                Clock.Play();
+
+                while (true)
+                {
+                    clockPosition = Clock.Position;
+                    renderIndex = MainBlockBuffer.IndexOf(clockPosition);
+                    renderBlock = MainBlockBuffer[renderIndex];
+                    hasRendered = false;
+
+                    // Check for out-of sync errors
+                    if (MainBlockBuffer.IsInRange(clockPosition) == false)
+                    {
+                        $"SYNC ERROR - Setting CLK from {Clock.Position.Debug()} to {MainBlockBuffer.RangeStartTime.Debug()}".Warn(typeof(MediaContainer));
+                        BufferBlocks(MaxPacketBufferLength / 4, true);
+                    }
+
+                    // render the frame if we have not rendered
+                    if (renderBlock.StartTime != lastRenderTime)
+                    {
+                        lastRenderTime = renderBlock.StartTime;
+                        hasRendered = true;
+                        RenderBlock(renderBlock, clockPosition, renderIndex);
+                    }
+
+                    // Add the next block if the conditions require us to do so:
+                    // If rendered, then we need to discard the oldest and add the newest
+                    // If the render index is greater than half, the capacity, add a new block
+                    while (hasRendered || renderIndex + 1 > MainBlockBuffer.Capacity / 2)
+                    {
+                        hasRendered = false;
+                        renderIndex = MainBlockBuffer.IndexOf(clockPosition);
+                        AddNextBlock();
+
+                        // Stop the loop if we can't reach the conditions.
+                        if (Frames.Count() == 0) break;
+                    }
+
+                    // Detect end of block rendering
+                    if (CanReadMoreBlocks == false && renderIndex == MainBlockBuffer.Count - 1)
+                    {
+                        // Rendered all and nothing else to read
+                        Clock.Pause();
+                        break;
+                    }
+
+                    Thread.Sleep(2);
+                }
+            });
+        }
+
+        #endregion
     }
 }
